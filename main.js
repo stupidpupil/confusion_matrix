bdb6276ee4bc3378a0ff21036c03c10bb359978c
Adam Watkins <adam@stupidpupil.co.uk>
2022-02-10 20:45:59 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+$(function(){
+
+	tinybind.formatters.percent = function(value){
+	  return "" + (value * 100).toFixed(1) + "%";
+	}
+
+	tinybind.bind(document.getElementById('body'), 
+		{
+			tp: 20, 
+			tn: 1820, 
+			fp: 180, 
+			fn: 10,
+			p:     function(){return parseInt(this.tp, 10) + parseInt(this.fn, 10)},
+			n:     function(){return parseInt(this.fp, 10) + parseInt(this.tn, 10)},
+
+			total: function(){return this.p() + this.n()},
+
+			predp: function(){return parseInt(this.fp, 10) + parseInt(this.tp, 10)},
+			predn: function(){return parseInt(this.fn, 10) + parseInt(this.tn, 10)},
+
+			prevalence:  function(){return this.p() / (this.total() )},
+			
+			sensitivity: function(){return parseInt(this.tp, 10) / this.p()},
+			specificity: function(){return parseInt(this.tn, 10) / this.n()},
+
+			accuracy: function(){return( (parseInt(this.tp, 10) + parseInt(this.tn, 10)) / (this.total()) )},
+			informedness: function(){return this.sensitivity() + this.specificity() - 1},
+
+			positive_predictive_value: function(){return parseInt(this.tp, 10) / this.predp()},
+			negative_predictive_value: function(){return parseInt(this.tn, 10) / this.predn()},
+
+			false_positive_rate: function(){return parseInt(this.fp, 10)/this.n()},
+			false_negative_rate: function(){return parseInt(this.fn, 19)/this.p()},
+
+
+		})	
+})
