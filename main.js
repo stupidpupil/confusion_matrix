ec0ab0f97c91d4663e0031d3aa32a476e1bcbeba
Adam Watkins <adam@stupidpupil.co.uk>
2022-02-10 21:03:16 +0000

    Add synonyms and save/restore state

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 $(function(){
 
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+  		get: (searchParams, prop) => searchParams.get(prop),
+	});
+
+
+
 	tinybind.formatters.percent = function(value){
 	  return "" + (value * 100).toFixed(1) + "%";
 	}
 
 	tinybind.bind(document.getElementById('body'), 
 		{
-			tp: 20, 
-			tn: 1820, 
-			fp: 180, 
-			fn: 10,
+			tp: params.tp || 20, 
+			tn: params.tn || 1820, 
+			fp: params.fp || 180, 
+			fn: params.fn || 10,
+
 			p:     function(){return parseInt(this.tp, 10) + parseInt(this.fn, 10)},
 			n:     function(){return parseInt(this.fp, 10) + parseInt(this.tn, 10)},
 
@@ -32,6 +39,12 @@ $(function(){
 			false_positive_rate: function(){return parseInt(this.fp, 10)/this.n()},
 			false_negative_rate: function(){return parseInt(this.fn, 19)/this.p()},
 
-
 		})	
+
+	$('input').change(function(e){
+	    var queryParams = new URLSearchParams(window.location.search);
+	    queryParams.set($(this).attr('name'), $(this).val());
+	    history.replaceState(null, null, "?"+queryParams.toString());
+	})
+
 })
